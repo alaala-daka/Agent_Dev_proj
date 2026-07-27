573748275cb2b235ae5145dbc820a7494396dc50
alaala-daka <zhouyiheng@mail.dlut.edu.cn>
2026-07-27 21:38:55 +0800

    完善Chroma库上传文件的追踪与管理

--- main_test.py
+++ main_test.py
@@ -7,5 +7,5 @@ load_dotenv()
 from Agent import Agent
 if __name__=='__main__':
     a=Agent()
-    for content in a.stream("告诉我在LangGraph基于开发过程中需要注意的点"):
+    for content in a.stream("客观叙述符号主义在人工智能领域发展的影响"):
         print(content,flush=True,end='')
